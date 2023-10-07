7590f02c79871b8e56bc3c879f1d660654d368e7
arina <arinasorokina825@gmail.com>
2023-10-07 12:08:29 +0300

    Styles the mobile header

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const toggle = document.querySelector(".js-toggle-button");
+const siteNav = document.querySelector(".nav__site-list");
+
+const onToggleClick = () => {
+  siteNav.classList.toggle("nav__site-list--open")
+}
+toggle.addEventListener('click', onToggleClick)
