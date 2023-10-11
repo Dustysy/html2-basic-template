ffdda0548515368b41cb0a10a9fa58dea2bc222a
arina <arinasorokina825@gmail.com>
2023-10-11 23:47:51 +0300

    Adds the slider's functions

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,12 @@
 /* в этот файл добавляет скрипты*/
 import { openMenu } from './open-menu.js'
+import { chooseSlide, showPrevSlide, showNextSlide } from './slider.js';
 // import { initRange } from './range.js';
 
 openMenu();
+
 // initRange();
+
+chooseSlide();
+showPrevSlide();
+showNextSlide();
