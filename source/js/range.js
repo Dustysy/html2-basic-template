21e0a98597dfed4e4626e001212f86b20e0dd0fc
arina <arinasorokina825@gmail.com>
2023-10-12 12:20:57 +0300

    Adds the range slider

--- source/js/range.js
+++ source/js/range.js
@@ -1,2 +1,36 @@
+const rangeSlider = document.querySelector(".range__wrapper");
+const minInput = document.querySelector(".js-price-min")
+const maxInput = document.querySelector(".js-price-max")
 
-export { initRange }
+const DEFAULT__VALUES = {
+  minPrice: 0,
+  maxPrice: 1000,
+  start: 0,
+  end: 900,
+  step: 1
+}
+
+const initRange = () => {
+  noUiSlider.create(rangeSlider, {
+    start: [DEFAULT__VALUES.start, DEFAULT__VALUES.end],
+    connect: true,
+    range: {
+        'min': DEFAULT__VALUES.minPrice,
+        'max': DEFAULT__VALUES.maxPrice
+    },
+    step: DEFAULT__VALUES.step
+  });
+}
+
+const onSliderUpdate = () => {
+  const sliderValue = rangeSlider.noUiSlider.get();
+  minInput.value = Math.floor(sliderValue[0]);
+  maxInput.value = Math.floor(sliderValue[1]);
+};
+
+
+const updateSlider = () => {
+  rangeSlider.noUiSlider.on('update', onSliderUpdate);
+};
+
+export { initRange, updateSlider }
--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,13 @@
 /* в этот файл добавляет скрипты*/
 import { openMenu } from './open-menu.js'
 import { chooseSlide, showPrevSlide, showNextSlide } from './slider.js';
-// import { initRange } from './range.js';
+import { initRange, updateSlider } from './range.js';
 
 openMenu();
 
 chooseSlide();
 showPrevSlide();
 showNextSlide();
+
+initRange();
+updateSlider();
